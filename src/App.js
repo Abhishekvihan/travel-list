df0fd5746cc78f6eb031cb86630883612b309313
abhishekvihan2 <abhishekvihan2@gmail.com>
2024-03-04 11:58:18 +0530

    lifiting items state in app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 12, packed: false },
-];
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: false },
+//   { id: 3, description: "Charger", quantity: 12, packed: false },
+// ];
 
 export default function App() {
+  const [items, setItem] = useState([]);
+
+  const handleAddItems = (item) => {
+    setItem((items) => [...items, item]);
+  };
   return (
     <div className="app">
       <Logo />
-      <Form />
-      <PackingList />
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} />
       <Stats />
     </div>
   );
@@ -20,7 +25,7 @@ function Logo() {
   return <h1> 🌴🌴Far Away🌴🌴</h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
   const [description, setDescription] = useState("");
   const [quantity, setQuantity] = useState(1);
 
@@ -36,7 +41,8 @@ function Form() {
       id: Date.now(),
     };
 
-    console.log(newItem);
+    onAddItems(newItem);
+
     setDescription("");
     setQuantity(1);
   }
@@ -65,11 +71,11 @@ function Form() {
   );
 }
 
-function PackingList() {
+function PackingList({ items }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
+        {items.map((item) => (
           <Item item={item} key={item.id} />
         ))}
       </ul>
