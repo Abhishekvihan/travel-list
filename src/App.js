ecb9566a74aac77e948eb5f3c35faf06d553903e
abhishekvihan2 <abhishekvihan2@gmail.com>
2024-03-04 12:36:03 +0530

    added deletion of items

--- src/App.js
+++ src/App.js
@@ -11,11 +11,14 @@ export default function App() {
   const handleAddItems = (item) => {
     setItem((items) => [...items, item]);
   };
+  const handleDeleteItem = (id) => {
+    setItem((items) => items.filter((item) => item.id !== id));
+  };
   return (
     <div className="app">
       <Logo />
       <Form onAddItems={handleAddItems} />
-      <PackingList items={items} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -71,25 +74,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({ items }) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} key={item.id} onDeleteItem={onDeleteItem} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>✖️</button>
+      <button onClick={() => onDeleteItem(item.id)}>✖️</button>
     </li>
   );
 }
