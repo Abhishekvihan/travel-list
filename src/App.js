66b0f4ce5c94f33982d4f77960428909c22447c4
abhishekvihan2 <abhishekvihan2@gmail.com>
2024-03-04 13:21:27 +0530

    added stats on the footer

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default function App() {
         onDeleteItem={handleDeleteItem}
         onToggleItems={handleToggleItem}
       />
-      <Stats />
+      <Stats items={items} />
     </div>
   );
 }
@@ -119,10 +119,23 @@ function Item({ item, onDeleteItem, onToggleItems }) {
     </li>
   );
 }
-function Stats() {
+function Stats({ items }) {
+  if (!items.length)
+    return (
+      <p className="stats">Start adding some items to your packing list🚀🚀</p>
+    );
+  const numItems = items.length;
+  const numPacked = items.filter((item) => item.packed).length;
+  const percentOfPacked = +(Math.round(numPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      🎒🎒You have X items on the list, you have already packed X x% items
+      <em>
+        {percentOfPacked === 100
+          ? "✈️✈️you got everthing! Ready to go ✈️✈️"
+          : `🎒🎒You have ${numItems} items on the list, you have already packed
+        ${numPacked} i.e ${percentOfPacked}% items`}
+      </em>
     </footer>
   );
 }
